5595679ecfdd1965bfd169ab03a74c80ca89d1bb
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-21 20:39:09 +0800

    [Red] Append new test cast - Times returns new instance of Doller

--- src/doller/doller_test.go
+++ src/doller/doller_test.go
@@ -6,8 +6,12 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	five.times(2)
-	if five.Amount != 10 {
-		t.Errorf("\nReturn the product of 5 and 2\nExpected: %d, Actual: %d", 10, five.Amount)
+	product := five.Times(2)
+	if product.Amount != 10 {
+		t.Errorf("\nReturn the product of 5 and 2\nExpected: %d, Actual: %d", 10, product.Amount)
+	}
+	product = five.Times(3)
+	if product.Amount != 15 {
+		t.Errorf("\nReturn the product of 5 and 3\nExpected: %d, Actual: %d", 15, product.Amount)
 	}
 }
