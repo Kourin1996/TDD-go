11933569d670b0217caf8c3c5f4a1a10b802ed14
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-21 21:02:50 +0800

    [Green & Blue] Fix codes of Equals function in Dollar to pass the test

--- src/dollar/dollar.go
+++ src/dollar/dollar.go
@@ -11,3 +11,7 @@ func NewDollar(amount int) *Dollar {
 func (this *Dollar) Times(multiplier int) *Dollar {
 	return &Dollar{Amount: this.Amount * multiplier}
 }
+
+func (this *Dollar) Equals(that *Dollar) bool {
+	return this.Amount == that.Amount
+}
--- src/dollar/dollar_test.go
+++ src/dollar/dollar_test.go
@@ -17,11 +17,11 @@ func TestMultiplication(t *testing.T) {
 }
 
 func TestEquality(t *testing.T) {
-	res1 := NewDollar(5).equals(NewDollar(5))
+	res1 := NewDollar(5).Equals(NewDollar(5))
 	if res1 != true {
 		t.Errorf("\nReturn if 5 of Dollar equals 5 of Dollar\nExpected: %t, Actual: %t", true, res1)
 	}
-	res2 := NewDollar(5).equals(NewDollar(6))
+	res2 := NewDollar(5).Equals(NewDollar(6))
 	if res2 == true {
 		t.Errorf("\nReturn if 5 of Dollar equals 6 of Dollar\nExpected: %t, Actual: %t", false, res2)
 	}
