1bc7c2d26ed393ba43254363004bcfa668c07b7e
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-25 21:38:17 +0800

    [Red] Fix test to be able to compare pair of different currency

--- src/money/dollar_test.go
+++ src/money/dollar_test.go
@@ -6,20 +6,21 @@ import (
 
 func TestDollarMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	if five.Times(2).Equals(NewDollar(10)) == false {
+	if Equals(five.Times(2), NewDollar(10)) == false {
 		t.Errorf("\nReturns if product of 5 and 2 equals 10, but return false")
 	}
-	if five.Times(3).Equals(NewDollar(15)) == false {
-		t.Errorf("\nReturns if product of 5 and 3 equals 15, but return false")
+	if Equals(five.Times(3), NewDollar(10)) == true {
+		t.Errorf("\nReturns if product of 5 and 3 equals 10, but return true")
 	}
 }
 
 func TestDollarEquality(t *testing.T) {
-	res1 := NewDollar(5).Equals(NewDollar(5))
+
+	res1 := Equals(NewDollar(5), NewDollar(5))
 	if res1 != true {
 		t.Errorf("\nReturn if 5 of Dollar equals 5 of Dollar\nExpected: %t, Actual: %t", true, res1)
 	}
-	res2 := NewDollar(5).Equals(NewDollar(6))
+	res2 := Equals(NewDollar(5), NewDollar(6))
 	if res2 == true {
 		t.Errorf("\nReturn if 5 of Dollar equals 6 of Dollar\nExpected: %t, Actual: %t", false, res2)
 	}
--- src/money/money_test.go
+++ src/money/money_test.go
@@ -5,11 +5,11 @@ import (
 )
 
 func TestDollarFrancEquality(t *testing.T) {
-	res1 := NewDollar(5).Equals(NewFranc(5))
+	res1 := Equals(NewDollar(5), NewFranc(5))
 	if res1 != false {
 		t.Errorf("\nReturn if 5 of Dollar equals 5 of Franc\nExpected: %t, Actual: %t", false, res1)
 	}
-	res2 := NewFranc(5).Equals(NewDollar(6))
+	res2 := Equals(NewFranc(5), NewDollar(6))
 	if res2 != false {
 		t.Errorf("\nReturn if 5 of Franc equals 6 of Dollar\nExpected: %t, Actual: %t", false, res2)
 	}
