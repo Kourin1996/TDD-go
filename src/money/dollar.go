d487bd177fd5669d8259545ad2a60c41d615bd9f
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-25 18:57:28 +0800

    [Blue] Create Money, base struct of Dollar and Franc

--- src/money/dollar.go
+++ src/money/dollar.go
@@ -1,17 +1,17 @@
 package money
 
 type Dollar struct {
-	amount int
+	Money
 }
 
 func NewDollar(amount int) *Dollar {
-	return &Dollar{amount: amount}
+	return &Dollar{Money: NewMoney(amount)}
 }
 
 func (this *Dollar) Times(multiplier int) *Dollar {
-	return &Dollar{amount: this.amount * multiplier}
+	return &Dollar{Money: NewMoney(this.amount * multiplier)}
 }
 
 func (this *Dollar) Equals(that *Dollar) bool {
-	return this.amount == that.amount
+	return this.Money.amount == that.Money.amount
 }
--- src/money/franc.go
+++ src/money/franc.go
@@ -1,17 +1,17 @@
 package money
 
 type Franc struct {
-	amount int
+	Money
 }
 
 func NewFranc(amount int) *Franc {
-	return &Franc{amount: amount}
+	return &Franc{Money: NewMoney(amount)}
 }
 
 func (this *Franc) Times(multiplier int) *Franc {
-	return &Franc{amount: this.amount * multiplier}
+	return &Franc{Money: NewMoney(this.amount * multiplier)}
 }
 
 func (this *Franc) Equals(that *Franc) bool {
-	return this.amount == that.amount
+	return this.Money.amount == that.Money.amount
 }
