c417de119f7ef9fbd5117aa520f4ec3f9ef7b7f7
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-25 21:38:55 +0800

    [Green & Blue] Fix codes to pass the test - Can use Equals between different currencies

--- src/money/money.go
+++ src/money/money.go
@@ -1,13 +1,22 @@
 package money
 
+type IMoney interface {
+	GetAmount() int
+	GetCurrencyCode() string
+}
+
+func Equals(a, b IMoney) bool {
+	return a.GetAmount() == b.GetAmount() && a.GetCurrencyCode() == b.GetCurrencyCode()
+}
+
 type Money struct {
 	amount int
 }
 
 func NewMoney(amount int) *Money {
-	return &Money{amount: amount}
+	return &Money{amount}
 }
 
-func (this *Money) Equals(that *Money) bool {
-	return this.amount == that.amount
+func (this *Money) GetAmount() int {
+	return this.amount
 }
