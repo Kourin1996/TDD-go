65c1076e7e0a825c5d298d7cf59144addeb65f2c
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-26 18:59:11 +0800

    [Green & Blue] Move Times in Dollar and Franc to Money

--- src/money/dollar_test.go
+++ src/money/dollar_test.go
@@ -15,7 +15,6 @@ func TestDollarMultiplication(t *testing.T) {
 }
 
 func TestDollarEquality(t *testing.T) {
-
 	res1 := Equals(NewDollar(5), NewDollar(5))
 	if res1 != true {
 		t.Errorf("\nReturn if 5 of Dollar equals 5 of Dollar\nExpected: %t, Actual: %t", true, res1)
