6ac002b307cf216d355eb8fac2447aaa5a3812d4
Yoshiki.T <yoshiki.takabayashi@gmail.com>
2019-11-28 20:11:04 +0800

    [Red] Add test - bank can reduce money

--- src/main_test.go
+++ src/main_test.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"testing"
-
-	"github.com/Kourin1996/TDD-go/src/bank"
-	"github.com/Kourin1996/TDD-go/src/expression"
-	"github.com/Kourin1996/TDD-go/src/money"
 )
 
 func TestSimpleAddition(t *testing.T) {
-	five := money.NewDollar(5)
+	five := NewDollar(5)
 	sum := five.Plus(five)
-	bank := bank.NewBank()
+	bank := NewBank()
 	reduced := bank.Reduce(sum, "USD")
-	if isEqual := money.Equals(reduced, money.NewDollar(10)); isEqual != true {
+	if isEqual := Equals(reduced, NewDollar(10)); isEqual != true {
 		t.Errorf("\nReturn sum of 5 Dollar and 5 Dollar equals 10 Dollar\nExpected: %t, Actual: %t", true, false)
 	}
 }
 
 func TestReduceSum(t *testing.T) {
-	sum := expression.NewSum(money.NewDollar(3), money.NewDollar(4))
-	bank := bank.NewBank()
+	sum := NewSum(NewDollar(3), NewDollar(4))
+	bank := NewBank()
 	result := bank.Reduce(sum, "USD")
-	if isEqual := money.Equals(money.NewDollar(7), result); isEqual != true {
+	if isEqual := Equals(NewDollar(7), result); isEqual != true {
 		t.Errorf("\nReturn sum of 3 Dollar and 4 Dollar equals 7 Dollar\nExpected: %t, Actual: %t", true, false)
+	}
+}
+
+func TestReduceMoney(t *testing.T) {
+	bank := NewBank()
+	result := bank.Reduce(NewDollar(1), "USD")
+	if isEqual := Equals(NewDollar(1), result); isEqual != true {
+		t.Errorf("\nReturn 1 Dollar equals 1 Dollar\nExpected: %t, Actual: %t", true, false)
 	}
 }
